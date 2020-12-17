652ea5986d6c25cc8ad2d394ad6ce29afdc1fe10
Achyut Dubey <achyut.dubey6.4@gmail.com>
2020-12-17 10:26:58 +0530

    Implementing Heapsort
    Input: (T, n_i, {x_i})
    2
    6
    12 11 20 5 16 7
    8
    15 24 16 22 5 20 40 8
    Output:
    5 7 11 12 16 20
    5 8 15 16 20 22 24 40

--- Heaps/Heap_Sort.c
+++ Heaps/Heap_Sort.c
@@ -3,19 +3,18 @@
 
 void swap(int *a, int *b)
 {
-   int  temp = *a;
-    *a = *b;
-    *b = temp;
+   int temp=*a;
+    *a=*b;
+    *b=temp;
 }
 
-
-void heapify(int arr[],int n,int i)
+void heapify(int arr[], int n, int i)
 {
     int large = i;
     int l=2*i+1;
     int r=2*i+2;
     
-    if(l<n && arr[l]<arr[large])
+    if(l<n && arr[l] < arr[large])
     {
         large = l;
     }
@@ -29,61 +28,63 @@ void heapify(int arr[],int n,int i)
     {
         swap(&arr[i], &arr[large]);
         
-        heapify(arr,n,large);
+        heapify(arr, n, large);
     }
 }
 
-void printHeap(int arr[],int n)
+void printHeap(int arr[], int n)
 {
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
         printf("%d ",arr[i]);
-    printf("\n");
+        
+     printf("\n");
 }
 
-void buildHeap(int arr[],int n)
+void buildHeap(int arr[], int n)
 {
-    int si = n/2 -1;
+    int si = ((n/2) -1);
     
     for(int i=si; i>=0; i--)
         heapify(arr,n,i);
-    
 }
 
-void sortHeap(int arr[], int ans[], int n, int *i)
+void sortHeap(int arr[], int ans[], int n, int* i)
 {
     if(n==0)
         return;
+        
     ans[(*i)++] = arr[0];
     
-    for(int j=1; j<n; j++)
+    for(int j=1;j<n;j++)
         arr[j-1]=arr[j];
+    
     n--;
     
-    buildHeap(arr,n);
-    sortHeap(arr,ans,n,i);
+    buildHeap(arr, n);
+    sortHeap(arr, ans, n, i);
 }
 
 
 int main() 
 {
-	int t;
-	scanf("%d",&t);
+	int testcase;
+	scanf("%d", &testcase);
 	
-	while(t--)
+	while(testcase--)
 	{
 	    int n,i=0;
 	    scanf("%d",&n);
 	    int arr[n],ans[n];
 	    
-	    for(int i=0;i<n;i++)
+	    for(int i=0; i<n; i++)
 	    {
-	        scanf("%d",&arr[i]);
+	        scanf("%d", &arr[i]);
 	    }
 	    
 	    buildHeap(arr, n);
-	    
 	    sortHeap(arr, ans, n, &i);
 	    printHeap(ans, n);
 	}
+	
 	return 0;
 }
