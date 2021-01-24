1172434c6b3ca4f2dd147eb65f94003ca581b56f
Achyut Dubey <achyut.dubey6.4@gmail.com>
2021-01-24 12:38:55 +0530

    Reversing a Linked List (Recursive)

---
+++ Linked List/ReverseLL_Recursive.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node* next;
+};
+
+Node* head = NULL;
+
+Node* create_Node(int data)
+{
+    Node* new_node = new Node();
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void push(int data)
+{
+    Node* new_node = create_Node(data);
+    Node* temp = head;
+    
+    if(head==NULL)
+        head = new_node;
+    else
+    {
+        while(temp->next)
+        temp = temp->next;
+        
+        temp->next = new_node;
+    }
+    
+}
+
+Node* reverseList(Node* head)
+{
+    if(head==NULL || head->next==NULL)
+    return head;
+    
+    Node* rest = reverseList(head->next);
+    head->next->next = head;
+    
+    head->next = NULL;
+    
+    return rest;
+}
+
+void printList()
+{
+    Node* temp = head;
+    
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    
+    cout<<"NULL";
+}
+
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    
+    printList();
+    cout<<endl;
+    head = reverseList(head);
+    printList();
+    
+    return 0;
+}
+
